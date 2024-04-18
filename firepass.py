8827e34a995eee8e7f48ab73f8b0d378a0242467
meetofleaf <meetofleaf@gmail.com>
2024-04-18 19:11:44 -0400

    Converted nested ifs to dictionary

--- firepass.py
+++ firepass.py
@@ -5,69 +5,13 @@ print("_____________\n")
 
 user_pass = input("Password: ")
 
+MAP = {'a':'@', 'A':'4', 'E':'3', 'g':'9', 'i':'I', 'I':'|', 'K':'|<', 'l':'1', 'L':'\_', 'o':'0', 'O':'0', 'p':'P', 's':'S', 'S':'5', 't':'+', 'T':'+', 'v':'\/', 'V':'\/', 'w':'vv', 'W':'VV', 'x':'><', 'X':'><', '0':')', '1':'!', '2':'@', '3':'#', '4':'$', '5':'%', '6':'^', '7':'&', '8':'*', '9':'('}
+
 def passgen(upass):
     chars = list(upass)
     for i in range(len(chars)):
-        if chars[i] == 'a':
-            chars[i] = '@'
-        elif chars[i] == 'A':
-            chars[i] = '4'
-        elif chars[i] == 'E':
-            chars[i] = '3'
-        elif chars[i] == 'g':
-            chars[i] = '9'
-        elif chars[i] == 'i':
-            chars[i] = 'I'
-        elif chars[i] == 'I':
-            chars[i] = '|'
-        elif chars[i] == 'K':
-            chars[i] = '|<'
-        elif chars[i] == 'l':
-            chars[i] = '1'
-        elif chars[i] == 'L':
-            chars[i] = '\_'
-        elif chars[i] == 'o' or chars[i] == 'O':
-            chars[i] = '0'
-        elif chars[i] == 'p':
-            chars[i] = 'P'
-        elif chars[i] == 's':
-            chars[i] = 'S'
-        elif chars[i] == 'S':
-            chars[i] = '5'
-        elif chars[i] == 't' or chars[i] == 'T':
-            chars[i] = '+'
-        elif chars[i] == 'v':
-            chars[i] = '\/'
-        elif chars[i] == 'V':
-            chars[i] = '\/'
-        elif chars[i] == 'w':
-            chars[i] = 'vv'
-        elif chars[i] == 'W':
-            chars[i] = 'VV'
-        elif chars[i] == 'x' or chars[i] == 'X':
-            chars[i] = '><'
-        elif chars[i] == '0':
-            chars[i] = ')'
-        elif chars[i] == '1':
-            chars[i] = '!'
-        elif chars[i] == '2':
-            chars[i] = '@'
-        elif chars[i] == '3':
-            chars[i] = '#'
-        elif chars[i] == '4':
-            chars[i] = '$'
-        elif chars[i] == '5':
-            chars[i] = '%'
-        elif chars[i] == '6':
-            chars[i] = '^'
-        elif chars[i] == '7':
-            chars[i] = '&'
-        elif chars[i] == '8':
-            chars[i] = '*'
-        elif chars[i] == '9':
-            chars[i] = '('
-        else:
-            continue
+        if chars[i] in MAP:
+            chars[i] = MAP[chars[i]]
     
     firepass = "".join(chars) + "_" + str(r.randint(0,1000))
     return firepass
